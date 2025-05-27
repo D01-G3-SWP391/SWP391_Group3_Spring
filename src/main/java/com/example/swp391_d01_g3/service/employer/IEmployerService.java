3541364156f12d0d9cc2c43a75ebbf0804a29543
Viết Tài <trantafi204@gmail.com>
2025-05-27 11:19:01 +0700

    Register Student Employer, BCrypt 27/5/2025

---
+++ src/main/java/com/example/swp391_d01_g3/service/employer/IEmployerService.java
@@ -0,0 +1,9 @@
+package com.example.swp391_d01_g3.service.employer;
+
+import com.example.swp391_d01_g3.model.Account;
+import com.example.swp391_d01_g3.model.Employer;
+
+public interface IEmployerService {
+    void save (Account account);
+    void saveEmployer(Employer employer);
+}
---
+++ src/main/java/com/example/swp391_d01_g3/service/student/IStudentService.java
@@ -0,0 +1,7 @@
+package com.example.swp391_d01_g3.service.student;
+
+import com.example.swp391_d01_g3.model.Account;
+
+public interface IStudentService {
+    Account save(Account account);
+}
