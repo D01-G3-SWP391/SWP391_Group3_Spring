66e8fd9a954ca1a1904b2b43d8306bf597a3b535
Viết Tài <trantafi204@gmail.com>
2025-06-19 11:33:53 +0700

    git update authentication 19/06/2025

---
+++ src/main/java/com/example/swp391_d01_g3/configuration/DataSeedingListener.java
@@ -0,0 +1,41 @@
+package com.example.swp391_d01_g3.configuration;
+import com.example.swp391_d01_g3.common.EncryptPasswordUtils;
+import com.example.swp391_d01_g3.model.Account;
+import com.example.swp391_d01_g3.service.security.IAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DataSeedingListener implements ApplicationListener<ContextRefreshedEvent> {
+    @Autowired
+    private IAccountService iAccountService;
+    
+    @Override
+    @Transactional
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        try {
+            if (iAccountService.findByEmail("admin@example.com") == null) {
+                Account admin = new Account(
+                        "Admin Name",
+                        "admin@example.com",
+                        EncryptPasswordUtils.EncryptPasswordUtils("123456"),
+                        "0123456789",
+                        Account.Role.admin,
+                        Account.Status.active
+                );
+                Account savedAdmin = iAccountService.save(admin);
+            } else {
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean supportsAsyncExecution() {
+        return ApplicationListener.super.supportsAsyncExecution();
+    }
+}
