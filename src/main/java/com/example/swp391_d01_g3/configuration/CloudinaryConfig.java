069f2f9fe28154d2e8f55a351201f2247f66bff3
Viết Tài <trantafi204@gmail.com>
2025-07-04 19:30:37 +0700

    add

--- src/main/java/com/example/swp391_d01_g3/configuration/CloudinaryConfig.java
+++ src/main/java/com/example/swp391_d01_g3/configuration/CloudinaryConfig.java
@@ -1,24 +1,32 @@
 package com.example.swp391_d01_g3.configuration;
 
 import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
-        // Đọc trực tiếp từ file .env
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"),
-                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
-                "api_secret", dotenv.get("CLOUDINARY_API_SECRET"),
-                "secure", true
-        ));
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        config.put("secure", "true");
+        return new Cloudinary(config);
     }
 } 
